15d442deab98f0749759b56dcaef17e5f18db60a
Demian Zolotukhin <demianzolotukhin@gmail.com>
2024-10-13 13:45:44 +0300

    robotPrototype solution

--- js.js
+++ js.js
@@ -897,3 +897,71 @@ newBag.setValue('Laptop', 'Dell latitude 5420');
 console.log(newBag.getValue('RAM'));
 console.log(newBag)
 newBag.getObjectInfo();
+
+
+const mainCore = {
+  getMainInfo() {
+    return `Robot ${this.name}, cpu version ${this.cpuVersion}`;
+  },
+
+  getAdditionalInfo() {
+    return `Update version: ${this.softwareVersion}`;
+  },
+
+  updateRobot(updateVersion) {
+    this.softwareVersion = updateVersion;
+
+    return `${this.name} updated to ${this.softwareVersion}`;
+  },
+};
+
+const navigationCore = {
+  getCoords() {
+    const { x, y } = this.coords;
+
+    return `x=${x} y=${y}`;
+  },
+
+  setTargetCoords(x = 0, y = 0) {
+    this.target.coords.x = x;
+    this.target.coords.y = y;
+  },
+};
+
+const movementCore = {
+  moveForward(step = 1) {
+    this.coords.y += step;
+  },
+
+  moveBack(step = 1) {
+    this.coords.y -= step;
+  },
+
+  moveLeft(step = 1) {
+    this.coords.x -= step;
+  },
+
+  moveRight(step = 1) {
+    this.coords.x += step;
+  },
+};
+
+const kerbin = {
+  name: 'Kerbin',
+  cpuVersion: 145.4,
+  softwareVersion: 23.45,
+  coords: {
+    x: 0,
+    y: 0,
+  },
+  target: {
+    coords: {
+      x: 0,
+      y: 0,
+    },
+  },
+};
+
+Object.setPrototypeOf(kerbin, movementCore);
+Object.setPrototypeOf(movementCore, navigationCore);
+Object.setPrototypeOf(navigationCore, mainCore);
