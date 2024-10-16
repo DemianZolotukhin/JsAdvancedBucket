5ff33baaaa84a1b0a5c0eda7c4539b87c2d95ddb
Demian Zolotukhin <demianzolotukhin@gmail.com>
2024-10-16 21:27:52 +0300

    BaseRobot solution

--- js.js
+++ js.js
@@ -1106,3 +1106,72 @@ console.log(amila.getInfo())
 console.log(camila.getInfo())
 console.log(camila.celebBirthday())
 console.log(camila.getInfo())
+
+
+
+class BaseRobot {
+  constructor(name, weight, coords = { x: 0, y: 0 }, chipVersion) {
+    this.name = name;
+    this.weight = weight;
+    this.coords = {
+      x: coords.x || 0,
+      y: coords.y || 0,
+    };
+    this.chipVersion = chipVersion;
+  }
+
+  goForward(step = 1) {
+    this.coords.y += step;
+  }
+
+  goBack(step = 1) {
+    this.coords.y -= step;
+  }
+
+  goRight(step = 1) {
+    this.coords.x += step;
+  }
+
+  goLeft(step = 1) {
+    this.coords.x -= step;
+  }
+
+  getInfo() {
+    return `Robot: ${this.name}, Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
+  }
+}
+
+class FlyingRobot extends BaseRobot {
+  constructor(name, weight, coords = { x: 0, y: 0, z: 0 }, chipVersion) {
+    super(name, weight, coords, chipVersion);
+    this.coords.z = coords.z || 0; 
+  }
+
+  goUp(step = 1) {
+    this.coords.z += step;
+  }
+
+  goDown(step = 1) {
+    this.coords.z -= step;
+  }
+}
+
+class DeliveryDrone extends FlyingRobot {
+  constructor(name, weight, coords, chipVersion, maxLoadWeight, currentLoad = null) {
+    super(name, weight, coords, chipVersion);
+    this.currentLoad = currentLoad; 
+    this.maxLoadWeight = maxLoadWeight; 
+  }
+
+ hookLoad(cargo) {
+    if (this.currentLoad === null && cargo.weight <= this.maxLoadWeight) {
+      this.currentLoad = cargo;
+    } 
+  };
+
+  unhookLoad() {
+    this.currentLoad = null; 
+  }
+}
+
+
